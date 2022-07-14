6122a64124f99eccfe57b3ebcee1a7407df5ef05
Manuel Galli <gmanuel89@gmail.com>
2022-07-14 12:47:33 +0200

    fixed parameter types

--- functions/get_csv_header.py
+++ functions/get_csv_header.py
@@ -1,5 +1,5 @@
 ## Get CSV header
-def get_csv_header(input_csv_file: list) -> list[str]:
+def get_csv_header(input_csv_file: list[list] | list[dict]) -> list[str]:
     if isinstance(input_csv_file[0], list):
         return input_csv_file[0]
     elif isinstance(input_csv_file[0], dict):
