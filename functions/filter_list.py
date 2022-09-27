72e14e18a7d372570a48993882e04b56911fd221
Manuel Galli <gmanuel89@gmail.com>
2022-09-27 15:54:44 +0200

    general function to filter a list

---
+++ functions/filter_list.py
@@ -0,0 +1,16 @@
+## Filter a list according to a provided list
+def filter_list(input_list: list[str], selected_list_entries: list[str]) -> list[str]:
+    # Initialise output variable
+    filtered_list = []
+    # Add only the item(s) of interest
+    if isinstance(selected_list_entries, list):
+        for sel in selected_list_entries:
+            for item in input_list:
+                if str(item).lower() == str(sel).lower():
+                    filtered_list.append(item)
+    else:
+        for item in input_list:
+            if str(item).lower() == str(selected_list_entries).lower():
+                filtered_list.append(item)
+    # return
+    return filtered_list
