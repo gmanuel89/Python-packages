9a57c3357536c61fda7793151e970b6065ae7069
Manuel Galli <gmanuel89@gmail.com>
2022-09-27 15:55:20 +0200

    removal of repeating slashes in path

--- functions/signalspki/get_response_content_from_tenant.py
+++ functions/signalspki/get_response_content_from_tenant.py
@@ -3,12 +3,16 @@ from functions.signalspki.formulate_headers_for_tenant_call import *
 import requests
 
 ## Get the content from the tenant
-def get_response_content_from_tenant(tenant_url: str, tenant_api_url_suffix: str, tenant_authentication: dict):
+def get_response_content_from_tenant(tenant_url: str, tenant_api_url_suffix: str, tenant_authentication: dict) -> dict:
     # Initialise output variable
     tenant_response_content = None
     # Formulate headers
     headers_api_call = formulate_headers_for_tenant_call(authentication_type=tenant_authentication.get('authentication_type'), authentication_parameters=tenant_authentication.get('authentication_parameters'))
-    # Formulate the API URL
+    # Formulate the API URL (remove the '/' from the tenant URL end and from the suffix beginning)
+    if tenant_url.endswith('/'):
+        tenant_url = tenant_url[0:len(tenant_url)-1]
+    if tenant_api_url_suffix.startswith('/'):
+        tenant_api_url_suffix = tenant_api_url_suffix[1:len(tenant_api_url_suffix)]
     tenant_api_url = tenant_url + '/' + tenant_api_url_suffix
     # Call the API
     try:
