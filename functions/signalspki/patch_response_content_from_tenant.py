3dfbb9212df2d5156dba4cba69db9a22172ab60c
Manuel Galli <gmanuel89@gmail.com>
2022-09-27 15:55:33 +0200

    added functions for POST and PATCH calls

---
+++ functions/signalspki/patch_response_content_from_tenant.py
@@ -0,0 +1,26 @@
+## Import libraries and functions
+from functions.signalspki.formulate_headers_for_tenant_call import *
+import requests
+
+## PATCH to a tenant and get the response from the tenant
+def patch_response_content_from_tenant(tenant_url: str, tenant_api_url_suffix: str, tenant_authentication: dict, payload_for_patch_request) -> dict:
+    # Initialise output variable
+    tenant_response_content = None
+    # Formulate headers
+    headers_api_call = formulate_headers_for_tenant_call(authentication_type=tenant_authentication.get('authentication_type'), authentication_parameters=tenant_authentication.get('authentication_parameters'))
+    # Formulate the API URL (remove the '/' from the tenant URL end and from the suffix beginning)
+    if tenant_url.endswith('/'):
+        tenant_url = tenant_url[0:len(tenant_url)-1]
+    if tenant_api_url_suffix.startswith('/'):
+        tenant_api_url_suffix = tenant_api_url_suffix[1:len(tenant_api_url_suffix)]
+    tenant_api_url = tenant_url + '/' + tenant_api_url_suffix
+    # Call the API
+    try:
+        tenant_response = requests.patch(tenant_api_url, headers=headers_api_call, data=payload_for_patch_request)
+        if tenant_response.ok:
+            tenant_response_content = tenant_response.json()
+    except:
+        print('Cannot PATCH the content to ' + tenant_url)
+        pass
+    # return
+    return tenant_response_content
