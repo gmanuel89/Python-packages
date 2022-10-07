a50f062f83612cef7c7149b9afa30cabbbf3926d
Manuel Galli <gmanuel89@gmail.com>
2022-10-07 13:00:14 +0200

    functions

--- functions/__init__.py
+++ functions/__init__.py
@@ -6,8 +6,3 @@ from functions.image_handling import *
 from functions.signalspki import *
 
 # App-specific functions
-from functions.generate_output_for_entities import *
-from functions.generate_output_for_measurement_types import *
-from functions.generate_output_with_endpoint_results import *
-from functions.generate_output_with_map_configurations import *
-from functions.generate_output_for_users import *
