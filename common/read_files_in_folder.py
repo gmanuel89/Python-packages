2fd7610f3497f0dccb6616542e8520c89f8510f0
Manuel Galli <gmanuel89@gmail.com>
2022-12-20 12:46:26 +0100

    new and updated functions

--- common/read_files_in_folder.py
+++ common/read_files_in_folder.py
@@ -1,7 +1,7 @@
 #####
 # Author: Manuel Galli
 # e-mail: gmanuel89@gmail.com / manuel.galli@perkinelmer.com
-# Updated date: 2022-11-30
+# Updated date: 2022-12-20
 #####
 
 ## Import libraries
@@ -10,17 +10,28 @@ import os
 ## Read files in a folder
 def read_files_in_folder(folder: str, file_types: list[str]) -> list[str]:
     # Initialise output
-    list_of_files = []
-    # List of files
-    list_of_all_files = os.listdir(folder)
+    files_in_folder = []
+    # List of files and folders
+    list_of_folders_and_files = os.listdir(folder)
     # Filter file types
     if file_types is not None and len(file_types) > 0:
-        for item in list_of_all_files:
+        for item in list_of_folders_and_files:
             for ext in file_types:
                 if item.endswith(ext):
-                    list_of_files.append(item)
+                    files_in_folder.append(item)
                     break
     else:
-        list_of_files = list_of_all_files
+        files_in_folder = list_of_folders_and_files
+    # Filter out directories from the entries
+    folder_entries = []
+    for entry in files_in_folder:
+        # try to list the files within the entry (if it fails, it is a file)
+        try:
+            files_in_item = os.listdir(folder + os.sep + entry)
+            folder_entries.append(entry)
+        except:
+            pass
+    for folder in folder_entries:
+        files_in_folder.remove(folder)
     # return
-    return list_of_files
+    return files_in_folder
---
+++ common/read_folders_in_folder.py
@@ -0,0 +1,25 @@
+#####
+# Author: Manuel Galli
+# e-mail: gmanuel89@gmail.com / manuel.galli@perkinelmer.com
+# Updated date: 2022-12-20
+#####
+
+## Import libraries
+import os
+
+## Read folders in a folder
+def read_folders_in_folder(folder: str) -> list[str]:
+    # Initialise output
+    folders_in_folder = []
+    # List of files and folders
+    list_of_folders_and_files = os.listdir(folder)
+    # Filter out directories from the entries
+    for entry in list_of_folders_and_files:
+        # try to list the files within the entry (if it fails, it is a file)
+        try:
+            files_in_item = os.listdir(folder + os.sep + entry)
+            folders_in_folder.append(entry)
+        except:
+            pass
+    # return
+    return folders_in_folder
