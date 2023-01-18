c278df8d73b2f66ac680083097ad396e934500ee
gmanuel89 <gmanuel89@gmail.com>
2023-01-18 10:24:00 +0100

    fixed split in one whole chunk

--- csv_handling/split_csv_file_content_into_chunks.py
+++ csv_handling/split_csv_file_content_into_chunks.py
@@ -1,19 +1,21 @@
 #####
 # Author: Manuel Galli
 # e-mail: gmanuel89@gmail.com / manuel.galli@perkinelmer.com
-# Updated date: 2022-12-22
+# Updated date: 2023-01-18
 #####
 
 ## Import libraries
 import math
 
 ## Function to split the original CSV file content into chunks (input can be either the number of 'lines' per chunk or the number of 'chunks' to obtain)
+# Worst case: a list of one element (i.e. one chunk) being the whole CSV content is returned, so that it is always a chunk-iterable list object
 def split_csv_file_content_into_chunks(csv_file_content: list[list] | list[dict], number_of_output_chunks=2, number_of_lines_per_chunk=10) -> list[list[list]] | list[list[dict]]:
     # Initialise output variable
     csv_file_content_split = []
     # If there is no CSV content or only one line
     if csv_file_content is None or len(csv_file_content) <= 1:
-        return csv_file_content
+        csv_file_content_split.append(csv_file_content)
+        return csv_file_content_split
     # If the format is a list of lists
     if csv_file_content is not None and isinstance(csv_file_content[0], list):
         # Store the header (to be placed at every csv)
@@ -27,7 +29,8 @@ def split_csv_file_content_into_chunks(csv_file_content: list[list] | list[dict]
         # The actual lines are beyond the header
         total_number_of_lines = len(csv_file_content)
     else:
-        return csv_file_content
+        csv_file_content_split.append(csv_file_content)
+        return csv_file_content_split
     # Calculate the number of lines per chunks
     if number_of_output_chunks is not None and number_of_output_chunks > 0: # mode == 'chunks'
         number_of_lines_per_chunk = math.ceil(total_number_of_lines / number_of_output_chunks)
