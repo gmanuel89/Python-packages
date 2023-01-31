51a821c46e5ad84c97c993a633a5465cc7f86e06
Manuel Galli <gmanuel89@gmail.com>
2023-01-31 17:22:15 +0100

    enabled multi-line by default on CSV upload

--- signalspki/inventa/upload_csv_content_to_dataset.py
+++ signalspki/inventa/upload_csv_content_to_dataset.py
@@ -1,7 +1,7 @@
 #####
 # Author: Manuel Galli
 # e-mail: gmanuel89@gmail.com / manuel.galli@perkinelmer.com
-# Updated date: 2022-12-05
+# Updated date: 2023-01-31
 #####
 
 ## Import libraries
@@ -21,9 +21,29 @@ def upload_csv_content_to_dataset(input_csv_file_content: str, input_data_name: 
     body, content_type = requests.models.encode_multipart_formdata({'file': (input_data_name, input_csv_file_content, 'text/plain')})
     # Upload the CSV file to Signals Inventa
     try:
-        file_upload_response = requests.post(tenant_url + 'job-service/projects/' + str(project_uid) + '/add-file?datasetUid=' + str(dataset_uid),
+        file_upload_response = requests.post(tenant_url + 'job-service/projects/' + str(project_uid) + '/add-file?datasetUid=' + str(dataset_uid),# + '&asDataset=true',
                                                 headers={'x-api-key': tenant_api_key, 'Content-Type': content_type},
                                                 data=body)
+        # Make the CSV as multi-line as default
+        if file_upload_response.ok:
+            uploaded_file_uid = file_upload_response.json().get('projectRevisionFileUid')
+            try:
+                csv_multiline_payload = {
+                    'dataSource': {
+                        'dataSourceType': 'delimited',
+                        'dataSourceInfo': {
+                            'delimiter': ',',
+                            'escapeChar': '"',
+                            'multiLine': True,
+                            'quoteChar': '"'
+                        }
+                    }
+                }
+                file_multiline_option_response = requests.patch(tenant_url + 'project-service/projects/' + str(project_uid) + '/revisions/0/files/' + str(uploaded_file_uid),# + '?asDataset=true',
+                                                                        headers={'x-api-key': tenant_api_key},
+                                                                        json=csv_multiline_payload)
+            except:
+                file_multiline_option_response = None
     except:
         file_upload_response = None
     # return
