6b947fbea2c5bf8513d79a5a14b8ebf157e1577e
Manuel Galli <gmanuel89@gmail.com>
2023-04-26 12:47:37 +0200

    improved retrieval of map ID from name

--- signalspki/inventa/get_map_id_from_name.py
+++ signalspki/inventa/get_map_id_from_name.py
@@ -1,27 +1,31 @@
 #####
 # Author: Manuel Galli
 # e-mail: gmanuel89@gmail.com / manuel.galli@perkinelmer.com
-# Updated date: 2022-12-21
+# Updated date: 2023-03-13
 #####
 
 ## Import libraries
 import requests
 
 ## Get map ID from name
-def get_map_id_from_name(map_name: str, tenant_url: str, tenant_api_key: str) -> str:
+def get_map_id_from_name(map_name: str, tenant_url: str, tenant_api_key: str, map_version=None) -> str:
     # Initialise output
     map_id = None
     # Fix tenant URL
-    if not tenant_url.endswith('/'):
-        tenant_url = tenant_url + '/'
+    if not tenant_url.endswith('/'): tenant_url = tenant_url + '/'
     # Retrieve content
     try:
-        map_list_response = requests.get(tenant_url + 'information-design-service/import-maps?limit=0',
+        map_list_response = requests.get(tenant_url + 'information-design-service/import-maps?limit=0&showDeleted=true' + '&name='+str(map_name),
                                         headers={'x-api-key': tenant_api_key})
         map_list_response_content = map_list_response.json()
         # Get map UID from list
         for mp in map_list_response_content:
-            if str(mp.get('name')).lower() == str(map_name).lower():
+            #if str(mp.get('name')).lower() == str(map_name).lower():
+            if map_version is not None and map_version != 0:
+                if mp.get('version') == map_version:
+                    map_id = mp.get('_id')
+                    break
+            else:
                 map_id = mp.get('_id')
     except:
         pass
