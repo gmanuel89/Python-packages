5f7ca0296d0c70773b830ced97f0ae675852d1c5
Manuel Galli <gmanuel89@gmail.com>
2023-07-17 17:56:08 +0200

    fixed function

--- signals/inventa/get_map_id_from_name.py
+++ signals/inventa/get_map_id_from_name.py
@@ -1,7 +1,7 @@
 #####
 # Author: Manuel Galli
 # e-mail: gmanuel89@gmail.com / manuel.galli@perkinelmer.com
-# Updated date: 2023-03-13
+# Updated date: 2023-07-17
 #####
 
 ## Import libraries
@@ -15,18 +15,20 @@ def get_map_id_from_name(map_name: str, tenant_url: str, tenant_api_key: str, ma
     if not tenant_url.endswith('/'): tenant_url = tenant_url + '/'
     # Retrieve content
     try:
-        map_list_response = requests.get(tenant_url + 'information-design-service/import-maps?limit=0&showDeleted=true' + '&name='+str(map_name),
+        map_list_response = requests.get(tenant_url + 'information-design-service/import-maps?limit=0&showDeleted=true',
                                         headers={'x-api-key': tenant_api_key})
         map_list_response_content = map_list_response.json()
         # Get map UID from list
         for mp in map_list_response_content:
-            #if str(mp.get('name')).lower() == str(map_name).lower():
-            if map_version is not None and map_version != 0:
-                if mp.get('version') == map_version:
+            if str(mp.get('name')) == str(map_name):
+                if map_version is not None and map_version != 0:
+                    if mp.get('version') == map_version:
+                        map_id = mp.get('_id')
+                        break
+                    else:
+                        map_id = mp.get('_id') # retrieve the latest map if no matching version is found
+                else:
                     map_id = mp.get('_id')
-                    break
-            else:
-                map_id = mp.get('_id')
     except:
         pass
     # return
