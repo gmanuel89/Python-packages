d1aa24199628d9a1670daa2b6735983e194970c2
Manuel Galli <gmanuel89@gmail.com>
2023-07-17 11:24:39 +0200

    removed data lake

--- data_lake/global_variables.py
+++
@@ -1,6 +0,0 @@
-## Global statement
-global tenant_url, tenant_authentication
-
-## Initialise variables
-tenant_url = 'https://http://inventa.pki-svcs.net/'
-tenant_authentication = {'authentication_type': 'api-key', 'authentication_parameters': {'api_key':'pv56di3d6v39utrf22bp002ecnl4mc9qqo80m5v6etiojvqdh17'}}
